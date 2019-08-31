7008178041f080e2081ab7fc66dbe12bb41b7dff
Farahani Samat <farahanis@gmail.com>
2019-08-31 17:17:37 +1000

    Add a new web source for quotes

--- quotes.py
+++ quotes.py
@@ -3,12 +3,22 @@ import random
 from bs4 import BeautifulSoup
 
 good_reads_quotes = 'https://www.goodreads.com/quotes/'
-tags = ['philosophy',
+gr_tags = ['philosophy',
         'science',
         'education',
         'inspirational',
         'time',
-        'life']
+        'life',
+        'wisdom']
+
+brainy_quote = 'https://www.brainyquote.com/'
+bq_tags = ['topics/leadership-quotes',
+           'topics/life-quotes',
+           'topics/success-quotes',
+           'quote_pictures',
+           'topics/funny-quotes',
+           'authors/lao-tzu-quotes',
+           'authors/confucius-quotes']
 
 
 class Quotes:
@@ -16,7 +26,14 @@ class Quotes:
         return
 
     def good_reads(self):
-        web_data = BeautifulSoup(requests.get(good_reads_quotes + 'tag/{}'.format(random.choice(tags))).text,
-                                 'html.parser').find_all(class_='quote mediumText ')
+        web_data = BeautifulSoup(requests.get(good_reads_quotes + 'tag/{}'.format(random.choice(gr_tags))).text,
+                                 'html.parser').find_all(class_='quoteDetails')
         text = '#goodreads #quote ' + random.choice(web_data).find(class_='quoteText').text.replace('\n', '').strip()
+        return text
+
+    def brainy(self):
+        web_data = BeautifulSoup(requests.get(brainy_quote + '{}'.format(random.choice(bq_tags))).text,
+                                 'html.parser').find_all(class_='clearfix')
+        text_items = [item.text for item in random.choice(web_data).find_all('a')]
+        text = '#brainyquote ' + ' - '.join(text_items)
         return text
